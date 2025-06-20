33746adb68d40132265ca948a06d37555af383e7
Petro Zhakun <p.zhakun@gmail.com>
2025-06-20 20:22:43 +0300

    add_hw_04_03_and_hw_04_04

---
+++ hw-04-03.py
@@ -0,0 +1,44 @@
+import sys
+from pathlib import Path
+from colorama import init, Fore, Style
+
+init(autoreset=True)  # Автоматичне скидання кольору після виводу
+
+def sort_key(path: Path):
+    # ім'я в нижньому регістрі
+    return path.name.lower()
+
+def print_tree(path: Path, indent=''):
+    # Обробка винятків
+    try:
+        items = sorted(path.iterdir(), key=sort_key)
+    except NotADirectoryError:
+        print(Fore.RED + f" Шлях не є директорією: {path}" + Style.RESET_ALL)
+        return
+    except FileNotFoundError:
+        print(Fore.RED + f" Шлях не існує: {path}" + Style.RESET_ALL)
+        return
+    except PermissionError:
+        print(Fore.RED + f" Немає доступу до: {path}" + Style.RESET_ALL)
+        return
+
+    # Візуалізація дерева файлової структури
+    for item in items:
+        if item.is_dir():
+            print(indent + Fore.BLUE + item.name + '/' + Style.RESET_ALL)
+            print_tree(item, indent + '    ')
+        else:
+            print(indent + Fore.GREEN + item.name + Style.RESET_ALL)
+
+
+# Основний блок, який виконується при запуску скрипта
+if name == '__main__':
+    try:
+        directory_path = Path(sys.argv[1])
+    except IndexError:
+        print("Вкажіть шлях до директорії як аргумент при запуску.")
+        print("Приклад: python tree_view.py /шлях/до/директорії")
+        sys.exit(1)
+
+    print(f"Структура директорії: {directory_path}")
+    print_tree(directory_path)
---
+++ hw-04-04.py
@@ -0,0 +1,68 @@
+# Функція обробки рядку, який ввів користувач
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+# Функція для додавання нового контакту
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+# Функція для зміни номеру існуючого контакту
+
+def change_contact(args, contacts):
+    name, new_phone = args
+    if name in contacts:
+        contacts[name] = new_phone
+        return f"Contact for {name} has been updated"
+    else:
+        print("Contact doesn't exist")
+
+# Функція для виводу номеру телефону контакта
+
+def show_number(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return f"{name}: {contacts[name]}"
+    else:
+        return "Contact not found"
+    
+# Функція для виводу усіх контактів
+
+def show_all_contacts(contacts):
+    result = []
+    for name, phone in contacts.items():
+        result.append(f"{name}: {phone}")
+    return "\n".join(result)
+
+# Основна функція, яка зчитує команди від юзера і виконує відповідні дії
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close","exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "show":
+            print(show_number(args, contacts))
+        elif command == "all":
+            print(show_all_contacts(contacts))
+        else:
+            print("Invalid command.")
+
+if name == "__main__":
+    main()
